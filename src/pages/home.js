41ff16f57b3c51578827c9e8ba1f793b3673c4bb
devthienphu <79859348+devthienphu@users.noreply.github.com>
2022-11-24 17:58:31 +0700

    add footer & category component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Category from '../components/Category/category';
+import Footer from '../components/Footer/footer';
 import Header from '../components/Header/header';
 import Product from '../components/Product/product';
 import Slider from '../components/Slider/slider';
@@ -9,6 +11,17 @@ const Home = () => {
             <Header/>
             <Slider/>
 
+            <div className="flex flex-col items-center justify-center py-4">
+                <p className="font-bold text-4xl "> What we can do</p>
+                <p className="">We look after your little family, we ensure that we will give the best treatment for your little family</p>
+                <div className="flex flex-row flex-wrap gap-x-8">
+                    <Category/>
+                    <Category/>
+                    <Category/>
+                    <Category/>
+                </div>
+            </div>
+
             <div className="flex flex-row flex-wrap gap-8 items-center justify-center">
             <Product/>
             <Product/>
@@ -18,6 +31,8 @@ const Home = () => {
             <Product/>
             <Product/>
             </div>
+
+            <Footer/>
             
             
         </>
